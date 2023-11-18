d2b83bd80d054cb4fbe30341bd79a8d1c802e5d5
Nazar <nazarskrinskyi@gmail.com>
2023-11-18 11:20:22 +0200

    set up controllers

---
+++ src/Controller/PricingController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PricingPlan;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class PricingController extends AbstractController
+{
+
+    public function __construct(private readonly ManagerRegistry $doctrine, private readonly EntityManagerInterface $entityManager) {}
+
+    #[Route('/pricing', name: 'pricing')]
+    public function index(): Response
+    {
+        $pricing = $this->doctrine->getRepository(PricingPlan::class)->findAll();
+        return $this->render('pricing/index.html.twig', [
+            'pricing' => $pricing,
+        ]);
+    }
+}
